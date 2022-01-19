83ebd8c68c63601d5c7027a16d03f226de25c7ce
Hobson Nascimento <hobsonbmn@gmail.com>
2022-01-19 15:27:36 -0300

    Atualização de Arquivos

--- exemplo.py
+++ exemplo.py
@@ -1,10 +1,8 @@
-from iqoptionapi.stable_api import IQ_Option
-from finta import TA 
-from time import time, sleep
-from datetime import datetime
-import pandas as pd
 
-API = IQ_Option ('hobsonbmn.trader@gmail.com','senha')
+from iqoptionapi.stable_api import IQ_Option
+import time
+
+API = IQ_Option ('hobsonbmn.trader@gmail.com','natalia12')
 API.connect()
 
 API.change_balance('PRACTICE') # PRACTICE / REAL
@@ -16,36 +14,14 @@ else:
     input('\n\n Aperte enter para sair')
     exit()
 
-def get_data (par,timeframe,periods = 200):
-    global API
-    
-    velas = API.get_candles(par,timeframe * 60 , periods,time())
-    
-    df = pd.DataFrame(velas)
-    df.rename(columns={"max":"high","min":"low"},inplace=True)
-    
-    return df
-def MovAvarDev(df,periods = 20):
-    src = TA.SSMA(df,periods)
-    
-    calc = df.iloc[-1]['close'] - src.iloc[-1]
-    
-    
-    return calc, 'green' if calc >= (df.iloc[-2]['close'] - src.iloc[-2]) else 'red'
+par ='EURUSD' 
+timeframe = 15
+ 
+payout = API.get_digital_payout(par)
+print(payout)
 
-def entrada (par , dir,timeframe):
-    global API
-    
-    print('\n Abrindo operação')
-    status,id = API.buy_digital_spot_v2(par,dir,timeframe)
-    
-    if status:
-        status = False 
-        while status == False:
-        
-    else:
-        print('Erro ao abrir a operação\n',id)
-        
-        print('\n')
-    
-    
+status, id = API.buy_digital_spot_v2(par,2.0,'put', 15)
+
+print(status ,id) 
+
+     
